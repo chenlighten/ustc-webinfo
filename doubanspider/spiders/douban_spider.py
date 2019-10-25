6c063d5e05a708b4de38c4810f03bafe81a285e5
xushichen <xushichen@sjtu.edu.cn>
2019-10-26 00:44:12 +0800

    Update

--- doubanspider/spiders/douban_spider.py
+++ doubanspider/spiders/douban_spider.py
@@ -13,8 +13,27 @@ class DoubanSpider(scrapy.Spider):
     
     def parse(self, response):
         music_name = response.xpath('//*[@id="wrapper"]/h1/span/text()').extract_first()
+        info = response.xpath('//*[@id="info"]')
+        music_type = info.re_first(r'流派\S*\s*(..)')
+        music_poster = response.xpath('//*[@id="mainpic"]/span/a/img/@src').get()
         
         item = DoubanspiderItem()
         item['music_name'] = music_name
+        item['music_url'] = response.url
+        item['music_type'] = music_type
+        item['music_poster'] = [music_poster]
+# //*[@id="comments"]/ul/li[1]/div/h3/span[2]/a
+# //*[@id="comments"]/ul/li[2]/div/h3/span[2]/a
+        short_remarks_list = []
+        for i in range(1, 6):
+            short_remark = response.xpath('//*[@id="comments"]//li[{:d}]'.format(i))
+            short_remarks_dict = {}
+            short_remarks_dict['id'] = short_remark.xpath('//div/h3/span[2]/a/text()').extract_first()
+            short_remarks_dict['content'] = short_remark.xpath('//div/p/span/text()').extract_first()
+            short_remarks_list.append(short_remarks_dict)
         
+        item['short_remarks'] = short_remarks_list
+        with open('feiyunzhixia.html', 'wb') as f:
+            f.write(response.body)
+
         yield item
